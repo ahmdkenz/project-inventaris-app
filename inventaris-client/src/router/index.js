6f5258a463ac38124aeed6c61e27ef18fe102e10
Yuki <gigibengal14@gmail.com>
2025-09-24 23:30:34 +0700

    Update vanilla JS to Vue.JS

--- inventaris-client/src/router/index.js
+++ inventaris-client/src/router/index.js
@@ -6,6 +6,16 @@ const routes = [
     name: 'Home',
     component: () => import('../views/dashboard/DashboardView.vue'),
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/auth/LoginView.vue'),
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/auth/RegisterView.vue'),
+  },
 ];
 
 const router = createRouter({
